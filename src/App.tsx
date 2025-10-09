a5918986d8d974b31461e62e7faa05a52968786e
Sadness <totushevr@yandex.ru>
2025-10-09 20:31:13 +0300

    created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styles from "./App.module.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import './swiper.css';
+import 'swiper/css/pagination';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App : React.FC = () : React.ReactElement => {
+    const [dates, setDates] = useState(1)
+    return (
+        <div className={styles.main}>
+            <h1 className={styles.main__h1}>Исторические даты</h1>
+            <div className={styles.main__years}>
+                <div className={styles.year}>1980</div>
+                <div className={styles.year}>1986</div>
+            </div>
+            <div className={styles.main__swiperBlock}>
+                <Swiper className="mySwiper">
+                    <SwiperSlide><h2>1980</h2><hr/><span>zxc</span></SwiperSlide>
+                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
+                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
+                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
+                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
+                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
 }
 
 export default App;
