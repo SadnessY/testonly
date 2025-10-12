443da5b22a60db6615d0b511622e690da0b3a9b4
Ruslan <totusher@yandex.ru>
2025-10-12 19:50:08 +0300

    сделано: отображение данных, переключение темы осталось: анимация при смене темы, круг с точками и анимации к нему

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,77 @@
 import React, {useState} from 'react';
 import styles from "./App.module.scss";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import './swiper.css';
-import 'swiper/css/pagination';
+import prevBtn from "./imgs/prevbtn.svg";
+import nextBtn from "./imgs/nextbtn.svg";
+import nextSlide from "./imgs/nextSlide.svg"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { gsap } from "gsap";
+import "swiper/css";
+
 
 const App : React.FC = () : React.ReactElement => {
-    const [dates, setDates] = useState(1)
+    const [dates, setDates] = useState(1)  //выбранная тема по номеру, не назвал theme, тк зачастую это слово используется для темы приложения, но можно че нить придумать интереснее
+    const [data, setData] = useState([      // можно загрузить данные с сервера, в моем варианте не используется setData, тк данные введены вручную, к тому же не знаю структуру данных на сервере, так что типизировать тоже не стал
+        { id: 1, name: 'Технологии', startYear: 1980, endYear: 1986, 
+            slides: [
+            {1980: 'Sinclare Research выпускают домашний компьютер ZX80'},
+            {1981: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1982: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1983: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1984: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1986: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'} 
+            ]
+        },
+        { id: 2, name: 'Кино', startYear: 1987, endYear: 1991,
+            slides: [
+            {1987: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1988: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1989: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1990: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            {1991: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'},
+            ]
+        }
+    ])
+    const handleBtnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (e.target === document.getElementsByClassName(styles.nav__arrow)[0]) {
+            setDates((dates) => dates - 1)
+        } else {
+            setDates((dates) => dates + 1)
+        }
+    }
     return (
-        <div className={styles.main}>
-            <h1 className={styles.main__h1}>Исторические даты</h1>
-            <div className={styles.main__years}>
-                <div className={styles.year}>1980</div>
-                <div className={styles.year}>1986</div>
-            </div>
-            <div className={styles.main__swiperBlock}>
-                <Swiper className="mySwiper">
-                    <SwiperSlide><h2>1980</h2><hr/><span>zxc</span></SwiperSlide>
-                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
-                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
-                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
-                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
-                    <SwiperSlide><h2>1980</h2><br/><span>zxc</span></SwiperSlide>
-                </Swiper>
+        <div className={styles.wind}>
+            <div className={styles.main}>
+                <h1 className={styles.main__h1}>Исторические даты</h1>
+                <div className={styles.main__verticalLine}></div>
+                <div className={styles.main__horizontalLine}></div>
+                <div className={styles.main__years}>
+                    <div className={styles.year}>{data.filter(item => item.id === dates)[0].startYear}</div>
+                    <div className={styles.year}>{data.filter(item => item.id === dates)[0].endYear}</div>
+                </div>
+                <div className={styles.nav}>
+                    <span className={styles.nav__counter}>0{dates}/06</span>
+                    <div className={styles.nav__btns}>
+                        <button onClick={(e) => handleBtnClick(e)} disabled={dates === 1} className={styles.nav__arrow}><img src={prevBtn} className={styles.prevArrow} alt='Предыдущая тема'></img></button>
+                        <button onClick={(e) => handleBtnClick(e)} disabled={dates === data.length} className={styles.nav__arrow}><img src={nextBtn} className={styles.nextArrow} alt='Следующая тема'></img></button> 
+                    </div>       
+                </div>
+                <div className={styles.swiperW}>
+                    <button className={styles.swiperButtonPrev}><img src={nextSlide} alt='Предыдущий слайд'></img></button>
+                    <Swiper
+                        modules={[Navigation]}
+                        spaceBetween={70}
+                        slidesPerView={3}
+                        watchOverflow
+                        navigation={{
+                            nextEl: `.${styles.swiperButtonNext}`,
+                            prevEl: `.${styles.swiperButtonPrev}`
+                        }}
+                    >
+                        {data.filter(item => item.id === dates)[0].slides.map(el => <SwiperSlide><div className={styles.swiperW__slide}><span>{Object.keys(el)[0]}</span><p>{Object.values(el)[0]}</p></div></SwiperSlide>)}
+                    </Swiper>
+                    <button className={styles.swiperButtonNext}><img src={nextSlide} alt='Следующий слайд'></img></button>
+                </div>
             </div>
         </div>
     )
